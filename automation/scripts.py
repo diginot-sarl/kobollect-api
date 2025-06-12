961c913a28a9ec1a6151cec24e481bdbe52c6446
Aaron-Laxis Dizele <aldizele@gmail.com>
2025-06-12 14:14:40 +0100

    feat: update user data handling in create_kobo_account and change import path for user data

--- automation/scripts.py
+++ automation/scripts.py
@@ -1,26 +1,26 @@
 import requests
 import json
 import logging
-from app.schemas import UserCreate
+# from app.schemas import UserCreate
 from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
-async def create_kobo_account(user_data: UserCreate):
+async def create_kobo_account(user_data: dict):
     """
     Route de test pour créer un compte KoboToolbox via l'API
     et vérifier les utilisateurs après 1 minute
     """
     KOBOTOOLBOX_URL = "http://kf.hidscollect.com"
     KOBOTOOLBOX_ADMIN_USER = "super_admin"
-    KOBOTOOLBOX_ADMIN_PASSWORD = "LIRV8Bfq5bkjJ3tspxhL"
+    KOBOTOOLBOX_ADMIN_PASSWORD = "3cvnyAlgLd8qylGVKe9G"
 
     user_info = {
-        "username": user_data.login,
+        "username": user_data.get('login'),
         "password": 123456,
-        "email": user_data.mail,
-        "first_name": user_data.prenom,
-        "last_name": user_data.nom
+        "email": user_data.get('mail'),
+        "first_name": user_data.get('prenom'),
+        "last_name": user_data.get('nom')
     }
 
     kobotoolbox_api_url = f"{KOBOTOOLBOX_URL}/api/v2/users/"
@@ -44,17 +44,17 @@ async def create_kobo_account(user_data: UserCreate):
 
 async def main():
     # Load users from JSON file
-    with open('assets/import_users.json', 'r') as f:
+    with open('automation/assets/import_users_v2.json', 'r') as f:
         users = json.load(f)
     
     # Process each user with progress bar
     for user in tqdm(users, desc="Creating Kobo accounts", unit="user"):
-        user_data = UserCreate(
-            login=user['username'],
-            prenom=user['metadata'].get('name', '').split(' ')[0] if user['metadata'].get('name') else '',
-            nom=user['metadata'].get('name', '').split(' ')[-1] if user['metadata'].get('name') else '',
-            mail=user['email']
-        )
+        user_data = {
+            "login": user['username'],
+            "prenom": user['metadata'].get('name', '').split(' ')[0] if user['metadata'].get('name') else '',
+            "nom": user['metadata'].get('name', '').split(' ')[-1] if user['metadata'].get('name') else '',
+            "mail": user['email']
+        }
         # Create Kobo account with await
         await create_kobo_account(user_data)
 
