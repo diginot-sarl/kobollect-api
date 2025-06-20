327c4eb61aa9fc08214d4291d453f26a2a6f8556
Aaron-Laxis Dizele <aldizele@gmail.com>
2025-06-20 10:42:13 +0100

    fix: update health check status to v1.8 and format selected IDs for better readability

--- app/main.py
+++ app/main.py
@@ -47,6 +47,6 @@ app.add_middleware(
 
 @app.get("/health", tags=["Health"])
 def health_check():
-    return {"status": "Hids Collect Working v1.7"}
+    return {"status": "Hids Collect Working v1.8"}
 
 app.include_router(routes, prefix='/api/v1')
